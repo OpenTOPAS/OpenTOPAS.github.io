9c87bc474f5033e33965a3e0f77af9330a6866b0
Ramon Ortiz Catalan <ramonortizcatalan@gmail.com>
2024-04-14 14:31:19 -0700

    First version of the webpage

---
+++ footer.js
@@ -0,0 +1,6 @@
+document.write('\
+\
+     <footer>2024. Open TOPAS official site. Last update: Apr 14, 2024.\
+    </footer>\
+\
+');
---
+++ navigation.js
@@ -0,0 +1,18 @@
+document.write('\
+\
+<nav>\
+<ul>\
+   <li><a href="/OpenTOPAS/webpage/index.html">Home</a></li>\
+   <li><a href="/OpenTOPAS/webpage/collaborators.html">Collaborators</a></li>\
+   <li><a href="/OpenTOPAS/webpage/publications.html">Publications</a></li>\
+   <li><a href="/OpenTOPAS/webpage/download.html" >Download</a></li>\
+   <li><a href="/OpenTOPAS/webpage/installation.html">Installation guide</a></li>\
+   <li><a href="https://opentopas.readthedocs.io/en/latest/" target="_blank">User guide</a></li>\
+   <li><a href="https://github.com/OpenTOPAS/topas/discussions" target="_blank">User forum</a></li>\
+   <li><a href="/OpenTOPAS/webpage/externalTools.html">External tools</a></li>\
+   <li><a href="/OpenTOPAS/webpage/news.html">News</a></li>\
+   <li><a href="/OpenTOPAS/webpage/contact.html"">Contact</a></li>\
+</ul>\
+</nav>\
+\
+');
