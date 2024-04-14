76e80a07cbc337e31d3c9a05f8ac0bba6774cef5
Ramon Ortiz Catalan <ramonortizcatalan@gmail.com>
2024-04-14 14:35:27 -0700

    grammar fix

--- navigation.js
+++ navigation.js
@@ -2,16 +2,16 @@ document.write('\
 \
 <nav>\
 <ul>\
-   <li><a href="/OpenTOPAS/webpage/index.html">Home</a></li>\
-   <li><a href="/OpenTOPAS/webpage/collaborators.html">Collaborators</a></li>\
-   <li><a href="/OpenTOPAS/webpage/publications.html">Publications</a></li>\
-   <li><a href="/OpenTOPAS/webpage/download.html" >Download</a></li>\
-   <li><a href="/OpenTOPAS/webpage/installation.html">Installation guide</a></li>\
+   <li><a href="/webpage/index.html">Home</a></li>\
+   <li><a href="/webpage/collaborators.html">Collaborators</a></li>\
+   <li><a href="/webpage/publications.html">Publications</a></li>\
+   <li><a href="/webpage/download.html" >Download</a></li>\
+   <li><a href="/webpage/installation.html">Installation guide</a></li>\
    <li><a href="https://opentopas.readthedocs.io/en/latest/" target="_blank">User guide</a></li>\
    <li><a href="https://github.com/OpenTOPAS/topas/discussions" target="_blank">User forum</a></li>\
-   <li><a href="/OpenTOPAS/webpage/externalTools.html">External tools</a></li>\
-   <li><a href="/OpenTOPAS/webpage/news.html">News</a></li>\
-   <li><a href="/OpenTOPAS/webpage/contact.html"">Contact</a></li>\
+   <li><a href="/webpage/externalTools.html">External tools</a></li>\
+   <li><a href="/webpage/news.html">News</a></li>\
+   <li><a href="/webpage/contact.html"">Contact</a></li>\
 </ul>\
 </nav>\
 \
