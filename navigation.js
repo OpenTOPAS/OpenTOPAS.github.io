b563518d333f3bce81021b39bfbf5854d3091b7a
RamonOrtizCatalan <122483063+RamonOrtizCatalan@users.noreply.github.com>
2024-04-15 14:09:05 -0700

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -2,16 +2,16 @@ document.write('\
 \
 <nav>\
 <ul>\
-   <li><a href="/OpenTOPAS.github.io/index.html">Home</a></li>\
+   <li><a href="./index.html">Home</a></li>\
    <li><a href="./collaborators.html">Collaborators</a></li>\
-   <li><a href="/OpenTOPAS.github.io/publications.html">Publications</a></li>\
-   <li><a href="/OpenTOPAS.github.io/download.html" >Download</a></li>\
-   <li><a href="/OpenTOPAS.github.io/installation.html">Installation guide</a></li>\
+   <li><a href="./publications.html">Publications</a></li>\
+   <li><a href="./download.html" >Download</a></li>\
+   <li><a href="./installation.html">Installation guide</a></li>\
    <li><a href="https://opentopas.readthedocs.io/en/latest/" target="_blank">User guide</a></li>\
    <li><a href="https://github.com/OpenTOPAS/topas/discussions" target="_blank">User forum</a></li>\
-   <li><a href="/OpenTOPAS.github.io/externalTools.html">External tools</a></li>\
-   <li><a href="/OpenTOPAS.github.io/news.html">News</a></li>\
-   <li><a href="/OpenTOPAS.github.io/contact.html"">Contact</a></li>\
+   <li><a href="./externalTools.html">External tools</a></li>\
+   <li><a href="./news.html">News</a></li>\
+   <li><a href="./contact.html"">Contact</a></li>\
 </ul>\
 </nav>\
 \
