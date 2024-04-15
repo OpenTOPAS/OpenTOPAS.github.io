a5d5426e76f77fbaa045a6cb20a8dfc66a078da1
RamonOrtizCatalan <122483063+RamonOrtizCatalan@users.noreply.github.com>
2024-04-15 13:20:37 -0700

    Update navigation.js
    Change of repository name in path.

--- navigation.js
+++ navigation.js
@@ -2,16 +2,16 @@ document.write('\
 \
 <nav>\
 <ul>\
-   <li><a href="/webpage/index.html">Home</a></li>\
-   <li><a href="/webpage/collaborators.html">Collaborators</a></li>\
-   <li><a href="/webpage/publications.html">Publications</a></li>\
-   <li><a href="/webpage/download.html" >Download</a></li>\
-   <li><a href="/webpage/installation.html">Installation guide</a></li>\
+   <li><a href="/OpenTOPAS.github.io/index.html">Home</a></li>\
+   <li><a href="/OpenTOPAS.github.io/collaborators.html">Collaborators</a></li>\
+   <li><a href="/OpenTOPAS.github.io/publications.html">Publications</a></li>\
+   <li><a href="/OpenTOPAS.github.io/download.html" >Download</a></li>\
+   <li><a href="/OpenTOPAS.github.io/installation.html">Installation guide</a></li>\
    <li><a href="https://opentopas.readthedocs.io/en/latest/" target="_blank">User guide</a></li>\
    <li><a href="https://github.com/OpenTOPAS/topas/discussions" target="_blank">User forum</a></li>\
-   <li><a href="/webpage/externalTools.html">External tools</a></li>\
-   <li><a href="/webpage/news.html">News</a></li>\
-   <li><a href="/webpage/contact.html"">Contact</a></li>\
+   <li><a href="/OpenTOPAS.github.io/externalTools.html">External tools</a></li>\
+   <li><a href="/OpenTOPAS.github.io/news.html">News</a></li>\
+   <li><a href="/OpenTOPAS.github.io/contact.html"">Contact</a></li>\
 </ul>\
 </nav>\
 \
