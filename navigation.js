4ed21a63c693868c4e194e9eec7df85d7c858e27
jschuemann <jschuemann@partners.org>
2024-08-28 22:08:42 -0400

    Update navigation.js
    Relink to Readthedocs

--- navigation.js
+++ navigation.js
@@ -8,7 +8,7 @@ document.write('\
    <li><a href="./publications.html">Publications</a></li>\
    <li><a href="./download.html" >Download</a></li>\
    <li><a href="./installation.html">Installation guide</a></li>\
-   <li><a href="https://ucsf.box.com/s/5g6n6uvi8goljpsku3z4gi6on959w4n3" target="_blank">User guide</a></li>\
+   <li><a href="https://opentopas.readthedocs.io/en/latest/" target="_blank">User guide</a></li>\
    <li><a href="https://github.com/OpenTOPAS/topas/discussions" target="_blank">User forum</a></li>\
    <li><a href="./userscollaboration.html">Collaborate</a></li>\
    <li><a href="./externalTools.html">External tools</a></li>\
